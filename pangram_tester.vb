34ec994257c603e53264f76b5cf69da0449b6ceb
Muhammad Muneeb <muhammadmuneeb10@gmail.com>
2019-10-13 19:29:35 +0500

    psuedo code and vb files added

---
+++ pangram_tester.vb
@@ -0,0 +1,52 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim alphabet_to_be_checked As String
+        Dim alphabet_being_checked As String
+        Dim continue_loop As Boolean
+        Dim counter As Integer
+        Dim counter_2 As Integer
+        Dim continue_2nd_loop As Boolean
+
+        alphabet_being_checked = ""
+        alphabet_to_be_checked = ""
+        continue_loop = True
+        str1 = ""
+        continue_2nd_loop = True
+
+        Console.Write("enter the sentence which is to be tested = ")
+        str1 = Console.ReadLine
+        str1 = LCase(str1)
+        If Len(str1) < 26 Then
+            Console.Write("the sentence cant be a pangram")
+            Console.ReadKey()
+            End
+        End If
+        For counter = 97 To 122
+            alphabet_to_be_checked = Chr(counter)
+            If continue_loop = True Then
+                For counter_2 = 1 To Len(str1)
+                    alphabet_being_checked = Mid(str1, counter_2, 1)
+                    If alphabet_being_checked <> alphabet_to_be_checked And continue_2nd_loop = True Then
+                        continue_2nd_loop = False
+                    ElseIf alphabet_being_checked = alphabet_to_be_checked Then
+                        continue_2nd_loop = True
+                        Exit For
+                    End If
+                Next
+                If continue_2nd_loop = False Then
+                    continue_loop = False
+                End If
+            End If
+        Next
+        If continue_loop = False Then
+            Console.Write("the sentence does not contain all the letters")
+            Console.ReadKey()
+            End
+        End If
+        Console.Write("the sentence is a pangram")
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ vowels_counter.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim counter, counter2, vowel_counter As Integer
+        Dim str1, str2, char1, char2 As String
+
+        str1 = ""
+        str2 = "aeiou"
+        counter = 0
+        char1 = ""
+        char2 = ""
+        vowel_counter = 0
+        counter2 = 0
+
+        Console.Write("enter the sentence to be tested = ")
+        str1 = Console.ReadLine
+        str1 = LCase(str1)
+        For counter = 1 To Len(str1)
+            char1 = Mid(str1, counter, 1)
+            For counter2 = 1 To Len(str2)
+                char2 = Mid(str2, counter2, 1)
+                If char1 = char2 Then
+                    vowel_counter = vowel_counter + 1
+                End If
+            Next
+        Next
+        Console.Write("there were ")
+        Console.Write(vowel_counter)
+        Console.Write(" vowels in the sentence.")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
